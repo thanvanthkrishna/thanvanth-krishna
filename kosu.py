0fa77befc661922202082561bb8b9a84c2d19834
thanvanthkrishna <thanvanthkrishna18@gmail.com>
2026-04-09 15:36:18 +0530

    Add files via upload

---
+++ kosu.py
@@ -0,0 +1,117 @@
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=a+b
+print(c)
+
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=a-b
+print(c)
+ 
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=a*b
+print(c)
+
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=a/b
+print(c)
+
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=a%b
+print(c)
+
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=a//b
+print(c)
+
+
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=a**b
+print(c)
+
+#Perimeter of Rectangle
+l=4
+b=3
+c=2*l+b
+print(" Perimeter of rectangle:",c)
+
+#Area of Rectangle
+l=5
+b=8
+c=l*b
+print(" Area of Rectangle:",c)
+
+#Perimeter of Square
+a=8
+c=4*a
+print(" Area Of square:",c)
+
+#area of square
+a=8
+c=a*a
+print("area of square:",c)
+
+#circumfernce of circle
+r=4
+a=2*3.14*r
+print("circumference of circle:",a)
+
+#area of circle
+r=5
+c=3.14*r
+print("area of circle:",c)
+
+
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("enter c:"))
+d=a+b+c
+print(" average is :",d/3)
+
+
+a=7
+b=4
+print(a==b)
+
+a=10
+b=5
+print(a<b)
+
+a=6
+b=9
+print(a>=b)
+
+a=6
+b=8
+c=a**0.5
+print(c)
+
+x=10
+x+=5
+x-=3
+x*=2
+x/=4
+x%=2
+x**=3
+print(x)
+
+a=8
+d=a**0.3333333333
+print(d)
+
+
+a=8523
+b=a%100
+print(b)
+
+
+a=8523
+b=a//100
+print(b)
+
+ 
